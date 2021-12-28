75c9226559fc1df197ddad5498e89646a7b3d01c
Simon Gottschlag <simon.gottschlag@xenit.se>
2021-12-28 10:33:05 +0100

    add google support

--- src/tokenservice.go
+++ src/tokenservice.go
@@ -1,10 +1,11 @@
 package main
 
 import (
+	"bytes"
 	"context"
+	"encoding/json"
 	"fmt"
 	"io"
-	"log"
 	"net/http"
 	"net/url"
 	"strings"
@@ -60,6 +61,7 @@ func NewTokenService(cfg config, kr *kubeReader) (*tokenService, error) {
 
 	internal.GET("/token/azure", ts.internalAzureTokenHttpHandler(jwks, cfg.ExternalIssuer))
 	internal.GET("/token/aws", ts.internalAwsTokenHttpHandler(jwks, cfg.ExternalIssuer))
+	internal.GET("/token/google", ts.internalGoogleTokenHttpHandler(jwks, cfg.ExternalIssuer))
 	external.GET("/.well-known/openid-configuration", metadataHttpHandler(cfg.ExternalIssuer))
 	external.GET("/jwks", jwksHttpHandler(jwks))
 
@@ -193,9 +195,6 @@ func getMicrosoftAccessToken(ctx context.Context, azureData azureAnnotations, in
 
 	defer res.Body.Close()
 
-	log.Printf("Azure response code: %d", res.StatusCode)
-	log.Printf("Azure response: %s", string(bodyBytes))
-
 	if res.StatusCode != 200 {
 		return nil, "", fmt.Errorf("received a non 200 status code: %d", res.StatusCode)
 	}
@@ -225,8 +224,6 @@ func getAwsAccessToken(ctx context.Context, awsData awsAnnotations, internalToke
 
 	remoteUrl.RawQuery = query.Encode()
 
-	fmt.Printf("AWS Request: %s\n", remoteUrl.String())
-
 	req, err := http.NewRequestWithContext(ctx, http.MethodPost, remoteUrl.String(), nil)
 	if err != nil {
 		return nil, "", err
@@ -244,8 +241,111 @@ func getAwsAccessToken(ctx context.Context, awsData awsAnnotations, internalToke
 
 	defer res.Body.Close()
 
-	log.Printf("AWS response code: %d", res.StatusCode)
-	log.Printf("AWS response: %s", string(bodyBytes))
+	if res.StatusCode != 200 {
+		return nil, "", fmt.Errorf("received a non 200 status code: %d", res.StatusCode)
+	}
+
+	contentType := res.Header.Get("Content-Type")
+	if contentType == "" {
+		return nil, "", fmt.Errorf("content type header is empty")
+	}
+
+	return bodyBytes, contentType, nil
+}
+
+// https://cloud.google.com/iam/docs/using-workload-identity-federation#authenticating_by_using_the_rest_api
+func getGoogleAccessToken(ctx context.Context, googleData googleAnnotations, internalToken string, subject string, aud string) ([]byte, string, error) {
+	stsReqBody := struct {
+		Audience           string `json:"audience"`
+		GrantType          string `json:"grantType"`
+		RequestedTokenType string `json:"requestedTokenType"`
+		Scope              string `json:"scope"`
+		SubjectTokenType   string `json:"subjectTokenType"`
+		SubjectToken       string `json:"subjectToken"`
+	}{
+		Audience:           strings.ReplaceAll(aud, "https://", "//"),
+		GrantType:          "urn:ietf:params:oauth:grant-type:token-exchange",
+		RequestedTokenType: "urn:ietf:params:oauth:token-type:access_token",
+		Scope:              googleData.scope,
+		SubjectTokenType:   "urn:ietf:params:oauth:token-type:jwt",
+		SubjectToken:       internalToken,
+	}
+
+	stsReqBodyJson, err := json.Marshal(stsReqBody)
+	if err != nil {
+		return nil, "", err
+	}
+
+	stsReq, err := http.NewRequestWithContext(ctx, http.MethodPost, "https://sts.googleapis.com/v1/token", bytes.NewReader(stsReqBodyJson))
+	if err != nil {
+		return nil, "", err
+	}
+
+	stsReq.Header.Set("Content-Type", "text/json; charset=utf-8")
+
+	stsRes, err := http.DefaultClient.Do(stsReq)
+	if err != nil {
+		return nil, "", err
+	}
+
+	stsBodyBytes, err := io.ReadAll(stsRes.Body)
+	if err != nil {
+		return nil, "", err
+	}
+
+	defer stsRes.Body.Close()
+
+	if stsRes.StatusCode != 200 {
+		return nil, "", fmt.Errorf("received a non 200 status code: %d", stsRes.StatusCode)
+	}
+
+	stsResBody := struct {
+		AccessToken     string `json:"access_token"`
+		IssuedTokenType string `json:"issued_token_type"`
+		TokenType       string `json:"token_type"`
+		ExpiresIn       int    `json:"expires_in"`
+	}{}
+
+	err = json.Unmarshal(stsBodyBytes, &stsResBody)
+	if err != nil {
+		return nil, "", err
+	}
+
+	remoteUrl, err := url.Parse(fmt.Sprintf("https://iamcredentials.googleapis.com/v1/projects/-/serviceAccounts/%s:generateAccessToken", googleData.gsa))
+	if err != nil {
+		return nil, "", err
+	}
+
+	reqBody := struct {
+		Scope []string `json:"scope"`
+	}{
+		Scope: []string{googleData.scope},
+	}
+
+	reqBodyJson, err := json.Marshal(reqBody)
+	if err != nil {
+		return nil, "", err
+	}
+
+	req, err := http.NewRequestWithContext(ctx, http.MethodPost, remoteUrl.String(), bytes.NewReader(reqBodyJson))
+	if err != nil {
+		return nil, "", err
+	}
+
+	req.Header.Set("Content-Type", "text/json; charset=utf-8")
+	req.Header.Set("Authorization", fmt.Sprintf("%s %s", stsResBody.TokenType, stsResBody.AccessToken))
+
+	res, err := http.DefaultClient.Do(req)
+	if err != nil {
+		return nil, "", err
+	}
+
+	bodyBytes, err := io.ReadAll(res.Body)
+	if err != nil {
+		return nil, "", err
+	}
+
+	defer res.Body.Close()
 
 	if res.StatusCode != 200 {
 		return nil, "", fmt.Errorf("received a non 200 status code: %d", res.StatusCode)
@@ -346,6 +446,44 @@ func (ts *tokenService) internalAwsTokenHttpHandler(jwks *jwksHandler, issuer st
 		}
 
 		responseData, contentType, err := getAwsAccessToken(c.Request.Context(), reqData, internalToken, subject)
+		if err != nil {
+			c.AbortWithError(http.StatusInternalServerError, err)
+			return
+		}
+
+		c.Data(http.StatusOK, contentType, responseData)
+	}
+}
+
+func (ts *tokenService) internalGoogleTokenHttpHandler(jwks *jwksHandler, issuer string) gin.HandlerFunc {
+	return func(c *gin.Context) {
+		subject, err := getSubjectFromClaims(c)
+		if err != nil {
+			c.AbortWithError(http.StatusInternalServerError, err)
+			return
+		}
+
+		namespace, serviceAccount, err := getNamespaceAndServiceAccountFromSubject(subject)
+		if err != nil {
+			c.AbortWithError(http.StatusInternalServerError, err)
+			return
+		}
+
+		reqData, err := ts.kr.getGoogleAnnotations(c.Request.Context(), namespace, serviceAccount)
+		if err != nil {
+			c.AbortWithError(http.StatusInternalServerError, err)
+			return
+		}
+
+		audience := fmt.Sprintf("https://iam.googleapis.com/projects/%s/locations/global/workloadIdentityPools/%s/providers/%s", reqData.projectNumber, reqData.poolId, reqData.providerId)
+
+		internalToken, err := newAccessToken(jwks, issuer, subject, audience)
+		if err != nil {
+			c.AbortWithError(http.StatusInternalServerError, err)
+			return
+		}
+
+		responseData, contentType, err := getGoogleAccessToken(c.Request.Context(), reqData, internalToken, subject, audience)
 		if err != nil {
 			c.AbortWithError(http.StatusInternalServerError, err)
 			return
